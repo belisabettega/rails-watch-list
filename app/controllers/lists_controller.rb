7b9e2573f97ef05b4d362e6d04c454b9f2cff440
belisa bettega <belisabettega@gmail.com>
2021-05-27 16:07:25 -0300

    implemented the create method to the list model

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -7,7 +7,24 @@ class ListsController < ApplicationController
   def show
   end
 
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(secure_params)
+    if @list.save
+      redirect_to lists_path
+    else
+      render 'new'
+    end
+  end
+
   private
+
+  def secure_params
+    params.require(:list).permit(:name)
+  end
 
   def find_list
     @list = List.find(params[:id])
