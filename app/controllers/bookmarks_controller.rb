65d2e468d3cad3b377ea2eea550cbc436ed1ae94
belisa bettega <belisabettega@gmail.com>
2021-05-27 17:25:31 -0300

    started new and create for the bookmark - problem with the instance variable @bookmark in the method new

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,23 @@
 class BookmarksController < ApplicationController
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(list: @list)
+  end
+
+  def create
+    @movie = Movie.find_by(title: params[:movie])
+    @new_bookmark = Bookmark.new(secure_params)
+    @new_bookmark.movie = @movie
+    if @new_bookmark.save
+      redirect_to list_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def secure_params
+    params.require(:bookmark).permit(:comment)
+  end
 end
