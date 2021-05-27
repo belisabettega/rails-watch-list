1b5f0964700a95862d80da057506dd4e6fdac62a
belisa bettega <belisabettega@gmail.com>
2021-05-27 17:58:27 -0300

    Tried to solve the error. Now I have to analyse method create in the model Bookmarks

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,15 +1,15 @@
 class BookmarksController < ApplicationController
   def new
     @list = List.find(params[:list_id])
-    @bookmark = Bookmark.new(list: @list)
   end
 
   def create
-    @movie = Movie.find_by(title: params[:movie])
+    @list = List.find(params[:list_id])
+    @movie = Movie.find_by(title: params[:bookmark][:movie])
     @new_bookmark = Bookmark.new(secure_params)
     @new_bookmark.movie = @movie
     if @new_bookmark.save
-      redirect_to list_path
+      redirect_to lists_path
     else
       render 'new'
     end
